830ee6486e987841a07fc6cb6f2530bf522c16e2
yonatan957 <yonatan22660@gmail.com>
2024-07-24 10:52:46 +0300

    Add project files.

---
+++ ctuba/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace ctuba
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            comboBox_year = new ComboBox();
+            comboBox_weekDays = new ComboBox();
+            comboBox_monthDays = new ComboBox();
+            comboBox_months = new ComboBox();
+            button_add = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // comboBox_year
+            // 
+            comboBox_year.FormattingEnabled = true;
+            comboBox_year.Items.AddRange(new object[] { "תשפ\"ד", "תשפ\"ה", "תשפ\"ו", "תשפ\"ז", "תשפ\"ח", "תשפ\"ט", "תש\"צ", "תשצ\"א", "תשצ\"ב", "תשצ\"ג" });
+            comboBox_year.Location = new Point(12, 42);
+            comboBox_year.Name = "comboBox_year";
+            comboBox_year.RightToLeft = RightToLeft.Yes;
+            comboBox_year.Size = new Size(151, 28);
+            comboBox_year.TabIndex = 0;
+            // 
+            // comboBox_weekDays
+            // 
+            comboBox_weekDays.FormattingEnabled = true;
+            comboBox_weekDays.Items.AddRange(new object[] { "ראשון", "שני", "שלישי", "רביעי", "חמישי", "ששי" });
+            comboBox_weekDays.Location = new Point(621, 42);
+            comboBox_weekDays.Name = "comboBox_weekDays";
+            comboBox_weekDays.RightToLeft = RightToLeft.Yes;
+            comboBox_weekDays.Size = new Size(151, 28);
+            comboBox_weekDays.TabIndex = 1;
+            // 
+            // comboBox_monthDays
+            // 
+            comboBox_monthDays.FormattingEnabled = true;
+            comboBox_monthDays.Items.AddRange(new object[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30" });
+            comboBox_monthDays.Location = new Point(426, 42);
+            comboBox_monthDays.Name = "comboBox_monthDays";
+            comboBox_monthDays.RightToLeft = RightToLeft.Yes;
+            comboBox_monthDays.Size = new Size(151, 28);
+            comboBox_monthDays.TabIndex = 2;
+            // 
+            // comboBox_months
+            // 
+            comboBox_months.FormattingEnabled = true;
+            comboBox_months.Items.AddRange(new object[] { "תשרי", "מרחשון", "כסלו", "טבת", "שבט", "אדר", "אדר הראשון", "אדר השני", "ניסן", "אייר", "סיון", "תמוז", "אב", "אלול" });
+            comboBox_months.Location = new Point(221, 42);
+            comboBox_months.Name = "comboBox_months";
+            comboBox_months.RightToLeft = RightToLeft.Yes;
+            comboBox_months.Size = new Size(151, 28);
+            comboBox_months.TabIndex = 3;
+            // 
+            // button_add
+            // 
+            button_add.Location = new Point(357, 376);
+            button_add.Name = "button_add";
+            button_add.Size = new Size(94, 29);
+            button_add.TabIndex = 4;
+            button_add.Text = "הוסף";
+            button_add.UseVisualStyleBackColor = true;
+            button_add.Click += button_add_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(72, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(37, 20);
+            label1.TabIndex = 5;
+            label1.Text = "שנה";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(278, 19);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 20);
+            label2.TabIndex = 6;
+            label2.Text = "חדש";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(468, 19);
+            label3.Name = "label3";
+            label3.Size = new Size(73, 20);
+            label3.TabIndex = 7;
+            label3.Text = "יום בחודש";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(663, 19);
+            label4.Name = "label4";
+            label4.Size = new Size(74, 20);
+            label4.TabIndex = 8;
+            label4.Text = "יום בשבוע";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button_add);
+            Controls.Add(comboBox_months);
+            Controls.Add(comboBox_monthDays);
+            Controls.Add(comboBox_weekDays);
+            Controls.Add(comboBox_year);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox comboBox_year;
+        private ComboBox comboBox_weekDays;
+        private ComboBox comboBox_monthDays;
+        private ComboBox comboBox_months;
+        private Button button_add;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+    }
+}
---
+++ ctuba/Form1.cs
@@ -0,0 +1,97 @@
+using System.Xml.Linq;
+
+namespace ctuba
+{
+    public partial class Form1 : Form
+    {
+        public Dictionary<string, string> _mydays = new Dictionary<string, string>();
+        public Dictionary<string, string> _myyears;
+        public Dictionary<string, string> _myweekDays;
+        public Form1()
+        {
+            InitializeComponent();
+            FillDicts();
+        }
+        public void FillDicts()
+        {
+            _mydays.Add("1", "יום אחד");
+            _mydays.Add("2", "שני ימים");
+            _mydays.Add("3", "שלשה ימים");
+            _mydays.Add("4", "ארבעה ימים");
+            _mydays.Add("5", "חמישה ימים");
+            _mydays.Add("6", "שישה ימים");
+            _mydays.Add("7", "שבעה ימים");
+            _mydays.Add("8", "שמונה ימים");
+            _mydays.Add("9", "תשעה ימים");
+            _mydays.Add("10", "עשרה ימים");
+            _mydays.Add("11", "אחד עשר יום");
+            _mydays.Add("12", "שנים עשר יום");
+            _mydays.Add("13", "שלשה עשר יום");
+            _mydays.Add("14", "ארבעה עשר ימים");
+            _mydays.Add("15", "חמישה עשר ימים");
+            _mydays.Add("16", "ששה עשר יום");
+            _mydays.Add("17", "שבעה עשר ימים");
+            _mydays.Add("18", "שמונה עשר ימים");
+            _mydays.Add("19", "תשעה עשר ימים");
+            _mydays.Add("20", "עשרים יום");
+            _mydays.Add("21", "עשרים ואחד ימים");
+            _mydays.Add("22", "עשרים ושתיים ימים");
+            _mydays.Add("23", "עשרים ושלושה ימים");
+            _mydays.Add("24", "עשרים וארבעה ימים");
+            _mydays.Add("25", "עשרים וחמישה ימים");
+            _mydays.Add("26", "עשרים וששה ימים");
+            _mydays.Add("27", "עשרים ושבעה ימים");
+            _mydays.Add("28", "עשרים ושמונה ימים");
+            _mydays.Add("29", "עשרים ותשעה ימים");
+            _mydays.Add("30", "שלושים ימים ימים");
+            _myyears = new Dictionary<string, string>()
+            {
+                        { "תשפ\"ד", "שנת חמשת אלפים ושבע מאות ושמנים וארבע לבריאת העולם" },
+                        { "תשפ\"ה", "שנת חמשת אלפים ושבע מאות ושמנים וחמש לבריאת העולם" },
+                        { "תשפ\"ו", "שנת חמשת אלפים ושבע מאות ושמנים ושש לבריאת העולם" },
+                        { "תשפ\"ז", "שנת חמשת אלפים ושבע מאות ושמנים ושבע לבריאת העולם" },
+                        { "תשפ\"ח", "שנת חמשת אלפים ושבע מאות ושמנים ושמונה לבריאת העולם" },
+                        { "תשפ\"ט", "שנת חמשת אלפים ושבע מאות ושמנים ותשע לבריאת העולם" },
+                        { "תש\"צ", "שנת חמשת אלפים ושמונה מאות ותשעים לבריאת העולם" },
+                        { "תשצ\"א", "שנת חמשת אלפים ושמונה מאות ותשעים ואחת לבריאת העולם" },
+                        { "תשצ\"ב", "שנת חמשת אלפים ושמונה מאות ותשעים ושתיים לבריאת העולם" },
+                        { "תשצ\"ג", "שנת חמשת אלפים ושמונה מאות ותשעים ושלוש לבריאת העולם" }
+            };
+            _myweekDays = new Dictionary<string, string>()
+            {
+                { "ראשון", "אחד" },
+                { "שני", "שניים" },
+                { "שלישי", "שלישי" },
+                { "רביעי", "רביעי" },
+                { "חמישי", "חמישי" },
+                { "ששי", "שישי" },
+            };
+        }
+
+        private void button_add_Click(object sender, EventArgs e)
+        {
+            string weekDay = comboBox_weekDays.Text.Trim();
+            string year = comboBox_year.Text.Trim();
+            string monthDay = comboBox_monthDays.Text.Trim();
+            string month = comboBox_months.Text.Trim();
+            if (string.IsNullOrEmpty(weekDay) || string.IsNullOrEmpty(year) || string.IsNullOrEmpty(month) || string.IsNullOrEmpty(monthDay))
+            {
+                MessageBox.Show("אנה הכנס את כל הערכים, כבוד הרב");
+                return;                
+            }
+            string ressult = $" ב{_myweekDays[weekDay]} בשבת {_mydays[monthDay]} לירח {month} {_myyears[year]}";
+            MessageBox.Show(ressult);
+            string path = "ctuba.xml";
+            //adding to xml
+            XDocument xDocument = XDocument.Load(path);
+            XElement xmlmonth = new XElement("Month", month);
+            XElement xmlyear = new XElement("Year", year);
+            XElement xmlweekDay = new XElement("Day", weekDay);
+            XElement xmlmonthDay = new XElement("DayMonth", monthDay);  
+            XElement xmlressult = new XElement("Ressult", ressult);
+            XElement query = new XElement("Query", xmlweekDay, xmlmonthDay, xmlmonth, xmlyear, xmlressult);
+            xDocument.Root.Add(query);
+            xDocument.Save(path);
+        }
+    }
+}
